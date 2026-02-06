bdcc0ed45c76fe756eb343b2008c44cd0a7062b2
Michael Gartner <mclicks@gmail.com>
2026-02-06 15:50:31 -0600

    dev

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import runExtension from "roamjs-components/util/runExtension";
 import { toggleStatsDrawer } from "./components/StatsDrawer";
+import { render as renderToast } from "roamjs-components/components/Toast";
 
 const COMMAND_LABEL = "Stats: Toggle Stats Drawer";
 export default runExtension(async ({ extensionAPI }) => {
+  if (process.env.NODE_ENV === "development") {
+    renderToast({
+      id: "stats-loaded",
+      content: "Stats extension loaded",
+      intent: "success",
+      timeout: 500,
+    });
+  }
+
   await extensionAPI.ui.commandPalette.addCommand({
     label: COMMAND_LABEL,
     callback: toggleStatsDrawer,
